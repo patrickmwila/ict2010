b5ec8e309c3a2cd94c61a1aae6e4fc2902387127
patrickmwila <2019037459@student.unza.zm>
2021-03-30 11:14:43 +0200

    initial commit

---
+++ 2017-18/lab1/Program1.java
@@ -0,0 +1,16 @@
+/**
+ * Author: Patrick Mwila
+ * StudentId: 2019037459
+ * Description: A simple program welcoming a new user
+ */
+
+public class Program1 // class header 
+{
+    // main method begins the execution of a java application
+    public static void main(String[] args)
+    {
+        // using print() method to welcome the user
+        System.out.print("Welcome to the system");
+    }
+
+}
