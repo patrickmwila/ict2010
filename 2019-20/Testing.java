6bdf132cc1b174cdb0f000e2ee2c43b72440eba9
patrickmwila <2019037459@student.unza.zm>
2021-04-25 15:43:34 +0200

    minor edits

--- 2019-20/Testing.java
+++ 2019-20/Testing.java
@@ -13,10 +13,18 @@ public class Testing
       System.out.println("This is a java application");
       JOptionPane.showMessageDialog(null, "Understanding syntax, semantics and paradigms " +
                                     "is the basics of programming.");
-
+/*
       System.out.print("Enter your studentID: ");
       int userID = keyboard.nextInt();
       JOptionPane.showMessageDialog(null, userID + " Welcome to the " + 
                                     "University of Zambia.");
+*/
+      System.out.print("Enter a number and i will print it's factors.");
+      int count;
+      for (count = 1; count <=10; count ++)
+      {
+          System.out.println("the square of " + count +" is " + count * count);
+          //System.out.println("i love programming!");
+      }
    }
 }
