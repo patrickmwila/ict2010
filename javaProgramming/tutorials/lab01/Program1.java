21925b2d339eb9df4c164d12e9d267fe7403d450
patrickmwila <patrickmwila.org@gmail.com>
2021-05-13 18:14:53 +0200

    minor edits

---
+++ javaProgramming/tutorials/lab01/Program1.java
@@ -0,0 +1,16 @@
+/**
+ * Author     : 2019037459
+ * Instructor : Phyela Mbewe 
+ * Date       : 13/05/2021
+ * Description: A program that displays text
+ */
+
+public class Program1 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to the system");
+    
+    }
+
+}
---
+++ javaProgramming/tutorials/lab01/Program2.java
@@ -0,0 +1,19 @@
+/**
+ * Author     : 2019037459
+ * Instructor : Phyela Mbewe
+ * Date       : 13/05/2021
+ * Description: A program that uses addition and multiplication to 
+ *              determine the value of a variable.
+ */
+
+public class Program2
+{
+    public static void main(String[] args)
+    {
+        int a = 10,           // declare a as int and give it value 10
+            b = 4,            // declare b as int and give it value 4
+            e = a*a + b*b;    // declare e as int and give it the result of (a*a + b*b)
+        
+        System.out.println(e); //display the value of e
+    }
+}
