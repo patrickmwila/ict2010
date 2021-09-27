582d20b2f2a3168578f4c4cc26b69f6e69e2f36f
patrickmwila <patrickmwila.org@gmail.com>
2021-09-27 18:23:33 +0200

    initial commit

---
+++ javaProgramming/assessment/assignment/ShareData.java
@@ -0,0 +1,21 @@
+/**
+ * Author      : Banda Esther - 2019002922 & Mwila Patrick - 2019037459
+ * Instructor  : Pheyela Mbewe
+ * Date        : 27-09-2021
+ * Description : A program that allows mobile phone users to send data to other users
+ */
+
+public class ShareData 
+{
+   public static void main (String [] args) 
+   {
+      System.out.println (
+         "Welcome to ZC mobile Data Sharing" + 
+         "\n---------------------------------" +
+         "\n1 - Send Data" +
+         "\n2 - Check Balance" +
+         "\n3 - Receive Free Gift" +
+         "\n0 - Exit"     
+      );
+   }
+}
